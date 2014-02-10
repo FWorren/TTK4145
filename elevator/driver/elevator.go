1ee531f31889b84bcbcc3dcde5000d3ec9642e56
FWorren <freworr@gmail.com>
2014-02-10 18:06:25 +0100

    Update

--- elevator/driver/elevator.go
+++ elevator/driver/elevator.go
@@ -2,6 +2,7 @@ package driver
 
 import (
 	"fmt"
+	"time"
 )
 
 type State_t int
@@ -61,9 +62,9 @@ func Elevator_statemachine() {
 func Elevator_wait() {
 	for {
 		OrderLogic_search_for_orders()
-		if OrderLogic_get_number_of_internal_orders() > 0 {State = RUN}
+		if OrderLogic_get_number_of_orders() > 0 {State = RUN}
 		if Elev_get_stop_signal() {State = STOPS}
-		if State != WAIT{break}
+		if State != WAIT || State == STOPS{break}
 	}
 }
 
@@ -79,22 +80,49 @@ func Elevator_run() {
 		}
 		if floor == Head_order.floor {
 			State = DOOR
+			Elev_set_speed(0)
 		}
-		if Elev_get_stop_signal() || Elev_get_obstruction_signal() {State = STOPS}
-		if State != RUN{break}
+		if Elev_get_stop_signal() || Elev_get_obstruction_signal() {
+			State = STOPS
+			Elev_set_speed(0)
+			break
+		}
+		if State != RUN {break}
 	}
 }
 
 func Elevator_door() {
+	OrderLogic_delete_current_order(Head_order.floor);
+	Elevator_clear_lights_current_floor(Head_order.floor);
+	if Elev_get_floor_sensor_signal() != -1 {Elev_set_door_open_lamp(1);}	
+	time.Sleep(3*time.Second)
 	for {
-		if State != DOOR{break}
+		if !Elev_get_obstruction_signal() {break}
+	}
+	if OrderLogic_get_number_of_orders() > 0 {
+		State = RUN
+		Elev_set_door_open_lamp(0) 
+	}else {
+		State = WAIT
+		Elev_set_door_open_lamp(0)
 	}
 }
 
 func Elevator_stop() {
+	fmt.Println("The elevator has stopped!\n1. If you wish to order a new floor, do so, or.\n2. Press Ctrl + c to exit program.\n");
+	Elevator_clear_all_lights()
+	Elev_set_stop_lamp(1)
+	OrderLogic_delete_all_orders()
+	if Elev_get_floor_sensor_signal() != -1{Elev_set_door_open_lamp(1)}
 	for {
-		if State != STOPS{break}
+		OrderLogic_search_for_orders()
+		if OrderLogic_get_number_of_orders() > 0{
+			State = RUN
+			break
+		}
 	}
+	Elev_set_stop_lamp(0)
+	Elev_set_door_open_lamp(0)
 }
 
 func Elevator_clear_lights_current_floor(current_floor int) {
--- elevator/driver/orderLogic.go
+++ elevator/driver/orderLogic.go
@@ -1,14 +1,18 @@
 package driver
 
+import "fmt"
+
 var command [N_FLOORS]int
 
-type order_state_t int
+type Order_state_t int
 
 const (
-	UP order_state_t = iota
+	UP Order_state_t = iota
 	DOWN
 	SET
 )
+
+var Order_state Order_state_t
 
 type Order struct {
 	floor int
@@ -36,27 +40,62 @@ func OrderLogic_search_for_orders() {
 }
 
 func OrderLogic_set_head_order() {
-	//OrderLogic_get_direction()
-	/*for {
-		switch {
+	OrderLogic_set_order_state()
+	for {
+		switch Order_state {
 			case UP:
-
+				OrderLogic_state_up()
 			case DOWN:
+				OrderLogic_state_down()
 			default:
-
+				fmt.Println("Error! No queue!\n")
+				Order_state = SET
 		}
-	}*/
+		if Order_state == SET {break}
+	}
+}
+
+func OrderLogic_set_order_state() {
+	if Previous_order.dir == 1 {
+		Order_state = UP
+	}else{
+		Order_state = DOWN
+	}
 }
 
 func OrderLogic_state_up() {
-	
+	if Previous_order.floor == N_FLOORS - 1 {
+		Order_state = DOWN;
+		return;
+	}
+	for i := Previous_order.floor+1; i < N_FLOORS; i++ {
+		if command[i] == 1 {
+			Head_order.floor = i
+			Head_order.dir = 1
+			Order_state = SET
+			return
+		}
+	}
+	Order_state = DOWN
 }
 
 func OrderLogic_state_down() {
-	
+	if Previous_order.floor == 0 {
+		Order_state = UP;
+		return;
+	}
+	for i := Previous_order.floor-1; i >= 0; i-- {
+		if command[i] == 1 {
+			Head_order.floor = i
+			Head_order.dir = -1
+			Order_state = SET
+			return
+		}
+	}
+	Order_state = UP
 }
 
-func OrderLogic_get_number_of_internal_orders() int {
+func OrderLogic_get_number_of_orders() int {
 	n_orders := 0
 	for i := 0; i < N_FLOORS; i++ {
 		if command[i] == 1 {
@@ -66,7 +105,21 @@ func OrderLogic_get_number_of_internal_orders() int {
 	return n_orders
 }
 
+func OrderLogic_delete_all_orders() {
+	for i := 0; i < N_FLOORS; i++ {
+		command[i] = 0
+	}
+	Head_order.floor = 0
+	Head_order.dir = 0
+}
+
 func OrderLogic_update_previous_order(floor int, direction int) {
 	Previous_order.floor = floor
 	Previous_order.dir = direction
+}
+
+func OrderLogic_delete_current_order(current_floor int) {
+	Previous_order.floor = Head_order.floor
+	Previous_order.dir = Head_order.dir
+	if command[current_floor] == 1 {command[current_floor] = 0 }
 }
